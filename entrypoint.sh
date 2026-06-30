311e7add3e755e6167b86b07777366d750fdcce1
telfordmisph <telford_mis_ph@astigp.com>
2026-07-01 07:19:02 +0800

    fix: remove gosu, let php-fpm handle worker privilege drop natively

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 chown -R 1000:33 /var/www/storage /var/www/bootstrap/cache 2>/dev/null
 chmod -R 775 /var/www/storage /var/www/bootstrap/cache 2>/dev/null
-exec gosu 1000:33 "$@"
+exec "$@"
